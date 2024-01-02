cc9ed06913c3e8154a9761e73a461dc592cf317f
Elaine Lin <elainewlin@gmail.com>
2024-01-01 16:34:53 -0800

    Add recipes

--- createPost.js
+++ createPost.js
@@ -1,32 +1,44 @@
-const rawRecipe = `Ingredients
-80 g black beans
-25 g peanut butter
-2 dates
-120 mL plant milk
-120 mL coffee
-1.5 tsp of cocoa powder
-1 tsp of flax seed
-Instructions`
+const rawRecipe = `Miyoko’s pancake mix
 
-const listify = (lines) => {
-  lines.split('\n').forEach(line => {
-    console.log(`  <li>${line}</li>`)
-  })
-}
+Ingredients
+480 grams all-purpose flour
+240 grams whole wheat flour
+54 grams sugar
+50 grams ground flaxseed
+40 grams baking powder
+5 grams salt
 
-const printRecipe = (recipe) => {
-  const [rawIngredients, rawInstructions] = recipe.split('Ingredients')[1].split('Instructions')
+Makes 8 pancakes
+For the pancakes
+120 grams of mix
+180 grams of plant milk
+1 tsp of vanilla extract
 
-  console.log('<b>Ingredients</b>')
-  console.log('<ul>')
-  listify(rawIngredients)
-  console.log('</ul>')
+Notes
+Better than most vegan pancakes. Easier to flip. Less gummy.
+`;
 
-  const instructions = rawInstructions.split('\n')
-  console.log('<b>Instructions</b>')
-  console.log('<ol>')
-  listify(instructions)
-  console.log('</ol>')
-}
+const listify = lines => {
+  lines.split("\n").forEach(line => {
+    console.log(`  <li>${line}</li>`);
+  });
+};
 
-listify(rawRecipe)
+const printRecipe = recipe => {
+  const [rawIngredients, rawInstructions] = recipe
+    .split("Ingredients")[1]
+    .split("Instructions");
+
+  console.log("<b>Ingredients</b>");
+  console.log("<ul>");
+  listify(rawIngredients);
+  console.log("</ul>");
+
+  const instructions = rawInstructions.split("\n");
+  console.log("<b>Instructions</b>");
+  console.log("<ol>");
+  listify(instructions);
+  console.log("</ol>");
+};
+
+listify(rawRecipe);
